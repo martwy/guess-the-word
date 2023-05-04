fcfd03b2113a5eb926a37a85b5b06ca33eed10c9
Arkadiusz <typowypolaczek@gmail.com>
2023-05-04 13:22:06 +0200

    validation + better code

--- src/App.js
+++ src/App.js
@@ -1,89 +1,100 @@
 import "./App.css";
 import WordLine from "./components/WordLine";
 import { useState, useEffect, useRef } from "react";
-function App() {
+const App = () => {
   const [words, setWords] = useState([
     {
       id: 0,
       word: "",
       letters: [],
       correct: [],
-      active: -1,
+      active: "withFocus",
     },
     {
       id: 1,
       word: "",
       letters: [],
       correct: [],
-      active: null,
+      active: "withoutFocus",
     },
     {
       id: 2,
       word: "",
       letters: [],
       correct: [],
-      active: null,
+      active: "withoutFocus",
     },
     {
       id: 3,
       word: "",
       letters: [],
       correct: [],
-      active: null,
+      active: "withoutFocus",
     },
     {
       id: 4,
       word: "",
       letters: [],
       correct: [],
-      active: null,
+      active: "withoutFocus",
     },
     {
       id: 5,
       word: "",
       letters: [],
       correct: [],
-      active: null,
+      active: "withoutFocus",
     },
   ]);
+  const [isEnd, setIsEnd] = useState(false);
+  const correctWord = "motyl";
 
-  const correctWord = "Motyl";
-
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      console.log("Enter works");
-      const activeIndex = words.findIndex((element) => element.active === -1);
-      console.log("Index przed: ", activeIndex);
-      if (activeIndex < 5) {
-        setWords((prevValue) => [
-          ...prevValue,
-          ((prevValue[activeIndex].active = null),
-          (prevValue[activeIndex + 1].active = -1)),
-        ]);
-        console.log(
-          "Index po: ",
-          words.findIndex((element) => element.active === -1)
-        );
-        checkWord();
-      } else if (activeIndex === 5) {
-        console.log("KONIEC GRY");
+  const handleKeyUp = (e) => {
+    if (!isEnd && e.key === "Enter") {
+      const activeIndex = words.findIndex(
+        (element) => element.active === "withFocus"
+      );
+      if (words[activeIndex].word) {
+        let newWord = checkWord();
+        newWord = [...newWord].map((word) => {
+          if (word.id === activeIndex) {
+            return { ...word, active: "withoutFocus" };
+          }
+          if (word.id === activeIndex + 1) {
+            return { ...word, active: "withFocus" };
+          }
+          return word;
+        });
+        setWords(newWord);
+        if (correctWord === words[activeIndex].word) {
+          setIsEnd(true);
+          alert("WYGRALES");
+        }
+        if (activeIndex === 5) {
+          setIsEnd(true);
+          alert("PRZEGRALES");
+        }
       }
     }
   };
 
   const handleGetLetter = (letter) => {
-    const activeIndex = words.findIndex((element) => element.active === -1);
-    setWords((prevValue) => [
-      ...prevValue,
-      ((prevValue[activeIndex].letters = [...letter]),
-      (prevValue[activeIndex].word = letter.join(""))),
-    ]);
+    const activeIndex = words.findIndex(
+      (element) => element.active === "withFocus"
+    );
+    const newWord = [...words].map((word) =>
+      word.id === activeIndex
+        ? { ...word, letters: [...letter], word: letter.join("") }
+        : word
+    );
+    setWords(newWord);
   };
 
   const checkWord = (word) => {
-    const activeIndex = words.findIndex((element) => element.active === -1);
+    const activeIndex = words.findIndex(
+      (element) => element.active === "withFocus"
+    );
     const splittedWord = [...correctWord.toLowerCase()];
-
     const correct = words[activeIndex].letters
       .map((element) => (splittedWord.includes(element) ? element : null))
       .map((element, index) =>
@@ -93,48 +104,28 @@ function App() {
           ? "correct-medium"
           : null
       );
-    setWords((prevValue) => [
-      ...prevValue,
-      (prevValue[activeIndex].correct = correct),
-    ]);
-    console.log(splittedWord);
+
+    const newWord = [...words].map((word) =>
+      word.id === activeIndex ? { ...word, correct: correct } : word
+    );
+    return newWord;
   };
 
+  const gamePanel = words.map((word) => (
+    <WordLine
+      key={word.id}
+      active={word.active}
+      correct={word.correct}
+      handle={handleGetLetter}
+      isEnd={isEnd}
+    />
+  ));
+
   return (
-    <div onKeyPress={handleKeyDown}>
-      <WordLine
-        marginek="przerwa"
-        active={words[0].active}
-        handle={handleGetLetter}
-        checked={words[0].correct}
-      />
-      <WordLine
-        active={words[1].active}
-        handle={handleGetLetter}
-        checked={words[1].correct}
-      />
-      <WordLine
-        active={words[2].active}
-        handle={handleGetLetter}
-        checked={words[2].correct}
-      />
-      <WordLine
-        active={words[3].active}
-        handle={handleGetLetter}
-        checked={words[3].correct}
-      />
-      <WordLine
-        active={words[4].active}
-        handle={handleGetLetter}
-        checked={words[4].correct}
-      />
-      <WordLine
-        active={words[5].active}
-        handle={handleGetLetter}
-        checked={words[5].correct}
-      />
+    <div className="container" onKeyUp={handleKeyUp}>
+      {gamePanel}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/WordLine.js
+++ src/components/WordLine.js
@@ -1,22 +1,19 @@
 import { useState, useEffect, useRef } from "react";
-function WordLine(props) {
+const WordLine = ({ handle, active, correct, isEnd }) => {
   const [letter, setLetter] = useState([]);
-  const handleKeyPress = (e) => {
-    console.log("Key: ", e.key);
-    if (letter.length <= 4 && e.key !== "Enter") {
-      setLetter((letterBefore) => [...letterBefore, e.key]);
-      console.log("Letter length: ", letter.length);
-      console.log("Akutalna tablica: ", letter);
-    } else if (e.key === "Enter") {
-      console.log(letter);
-      props.handle(letter);
-    }
-  };
 
   const handleKeyDown = (e) => {
-    if (e.key === "Backspace") {
-      console.log("Backspace works");
-      setLetter([...letter.slice(0, letter.length - 1)]);
+    if (!isEnd) {
+      if (letter.length <= 4 && e.key !== "Enter" && e.key !== "Backspace") {
+        const newLetter = [...letter, e.key];
+        setLetter(newLetter);
+      } else if (e.key === "Enter") {
+        if (letter.length === 5) handle(letter);
+        else alert("SLOWO NIE ISTNIEJE");
+      } else if (e.key === "Backspace") {
+        const deleteLastLetter = [...letter.slice(0, letter.length - 1)];
+        setLetter(deleteLastLetter);
+      }
     }
   };
 
@@ -28,19 +25,28 @@ function WordLine(props) {
 
   return (
     <div
-      className={`word-box ${props.marginek}`}
-      onKeyPress={handleKeyPress}
+      className="word-box"
       onKeyDown={handleKeyDown}
       ref={ref}
-      tabIndex={props.active}
+      tabIndex={active === "withFocus" ? -1 : active}
     >
-      <div className={`letter-box ${props.checked[0]}`}>{letter[0]}</div>
-      <div className={`letter-box ${props.checked[1]}`}>{letter[1]}</div>
-      <div className={`letter-box ${props.checked[2]}`}>{letter[2]}</div>
-      <div className={`letter-box ${props.checked[3]}`}>{letter[3]}</div>
-      <div className={`letter-box ${props.checked[4]}`}>{letter[4]}</div>
+      <div className={`letter-box ${correct ? correct[0] : ""}`}>
+        {letter[0]}
+      </div>
+      <div className={`letter-box ${correct ? correct[1] : ""}`}>
+        {letter[1]}
+      </div>
+      <div className={`letter-box ${correct ? correct[2] : ""}`}>
+        {letter[2]}
+      </div>
+      <div className={`letter-box ${correct ? correct[3] : ""}`}>
+        {letter[3]}
+      </div>
+      <div className={`letter-box ${correct ? correct[4] : ""}`}>
+        {letter[4]}
+      </div>
     </div>
   );
-}
+};
 
 export default WordLine;
